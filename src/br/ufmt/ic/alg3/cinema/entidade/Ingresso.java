29e5265013621a4c57bc4031afb273c9125b66d0
CodeArch <henrique.peixoto1512@gmail.com>
2020-09-29 20:37:56 -0400

    rename package

--- src/br/ufmt/ic/alg3/cinema/entidade/Ingresso.java
+++ src/br/ufmt/ic/alg3/cinema/entidade/Ingresso.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.ufmt.ic.alg3.cinema;
+package br.ufmt.ic.alg3.cinema.entidade;
 
 import java.math.BigDecimal;
 
