6875966e0f4cb479f2f7ab9679da7532bbe5f42b
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2020-10-19 15:19:58 -0400

    Mudar o Look and Feel para Nimbus, se disponivel

--- src/cinema/Main.java
+++ src/cinema/Main.java
@@ -1,6 +1,10 @@
 package cinema;
 
 import br.ufmt.ic.alg3.cinema.telas.JFramePrincipal;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -12,7 +16,25 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        /* Código para alterar o Look and Feel para Nimbus. O Look and Feel 
+            padrão não me agradou muito */
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
         JFramePrincipal tela = new JFramePrincipal();
         tela.setVisible(true);
